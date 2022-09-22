18d8f3e21d0daa7fa5353b919868e15d0ce6d79a
PacoTercero <106634146+PacoTercero@users.noreply.github.com>
2022-09-22 09:13:35 -0300

    Add files via upload

---
+++ src/components/Kids.vue
@@ -0,0 +1,23 @@
+<template>
+   <div> 
+    <input type="number" name="kids" v-model='kids'> E
+    <p> {{kids}}</p>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+   name : 'Kids',
+    data(){
+        return{
+        kids : 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ListarRecados.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="listar-recados">
+
+        <div class="recado">
+
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet,
+             neque quidem accusantium suscipit consectetur sed eum ab quos saepe totam voluptates labore odit nihil esse quibusdam, a voluptatem animi soluta!
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'ListarRecados'
+}
+</script>
+
+<style scoped>
+.listar-recados {
+    display: flex;
+}
+
+.recado {
+    width: 250px;
+    border: solid 1px #ccc;
+    border-radius: 5px;;
+    padding: 25px;
+}
+</style>
