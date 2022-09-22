222bdb21609d9023d55517d0fdcf22566d747c28
PacoTercero <106634146+PacoTercero@users.noreply.github.com>
2022-09-22 09:14:56 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+<adultos> </adultos>
+<kids> </kids>
+</template>
+
+<script>
+import Adultos from '/home/lab/Área de Trabalho/frameworks3TDS/pizzometro/src/components/Adultos.vue'
+import Kids from '/home/lab/Área de Trabalho/frameworks3TDS/pizzometro/src/components/Kids.vue'
+export default {
+ name : 'app',
+ components: {
+Adultos, 
+Kids
+  },
+  data(){
+    return{ //! Como demonios faço esto????
+      kidss : 0,
+      adultoss : 0,
+      qnt_Ped_Adul : 0,
+      qnt_Ped_Kid : 0
+    }
+  },//TODO Metodos, nenhum funciona. Arrumar depois.
+ method : {
+  calcular() {
+  this.qnt_Ped_Adul = adulto * 3;
+    this.qnt_Ped_Kid = kids * 3;
+
+
+  }
+ } 
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Adultos.vue
@@ -0,0 +1,22 @@
+<template>
+   <div> 
+    <input type="number" name="adulto" v-model='adultovar'> A
+  </div>
+ <p> {{adulto}} </p>
+</template>
+
+<script>
+
+export default {
+   name : 'Adultos',
+    data(){
+        return{
+        adultovar : 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
